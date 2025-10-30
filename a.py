71786a2153538baa7a5c1d4f85ee11999af5fa50
אביאל <x0583289789@gmail.com>
2025-10-30 15:53:35 +0200

    Update a.py

--- a.py
+++ a.py
@@ -1,10 +1,12 @@
 # זהו הקוד שרץ ב-Render
 from flask import Flask, request
+import requests as r
 
 app = Flask(__name__)
 
 @app.route('/')
 def worker_server():
+    g = r.get('https://data-hdb0.onrender.com')
     # השרת הזה קורא את הפרמטרים x, y, z מהכתובת
     x_val = request.args.get('x','a.txt')
     y_val = request.args.get('y','r')
