1a3ff09119885320d8acee04ef018419bac0a826
אביאל <x0583289789@gmail.com>
2025-11-04 12:05:33 +0200

    Update a.py

--- a.py
+++ a.py
@@ -4,19 +4,26 @@ import requests as r
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def worker_server():
     g = r.get('https://data-hdb0.onrender.com')
-    # השרת הזה קורא את הפרמטרים x, y, z מהכתובת
-    x_val = request.args.get('x','a.txt')
-    y_val = request.args.get('y','r')
-    z_val = request.args.get('z','a')
-    with open(x,y) as f:
-        kkk="kkk"    
-    if y=='w':
-        f.write(z)
-        f=''
-    else:
-        f.read()
-    
-    return f"{f}"
+    all = request.args if request.method == 'GET' else request.post
+    x = all.get('x')
+    y = all.get('y')
+    z = all.get('z',1)
+    if x and y and z:
+        with open(x,y) as f:
+            kkk="kkk"    
+        if y=='w':
+            f.write(z)
+            f=''
+        else:
+            f=f.read()
+        
+        return f"{f}"
+@app.route('/FFFFF', methods=['GET','POST'])
+def a():
+    all = request.args if request.method == 'GET' else request.post
+    pas = all.get('pas')
+    if pas != 55555666665555588888:
+        return 'sss'
